6dcb8f65c71df46ca7fb6eb3ac213ad92b4e4889
gleversonrafael <gleverson_rafael@hotmail.com>
2025-01-09 16:39:23 -0300

    09/01 - update later

--- scripts/mobmenu.js
+++ scripts/mobmenu.js
@@ -2,15 +2,22 @@
 const mButton = document.querySelector("span#mIcon");
 const mm = document.getElementById("mobMenu");
 const mmSec = document.querySelector("section#mobMenu-sec");
-
-
+let changeAllow = 0;
 
 // events
 // menu fix
 window.addEventListener("resize", function () {
-    if(window.width >= 768) {
+    if(window.innerWidth >= 768 && mm.classList.contains("mmActive")) {
+        changeAllow = 1;
+
         mm.classList.remove("mmActive");
         mmSec.classList.remove("mmActive");
+    
+    } else if(window.innerWidth < 768 && changeAllow == 1) {
+        mm.classList.add("mmActive");
+        mmSec.classList.add("mmActive");
+
+        changeAllow = 0;
     }
     
 });
